9d4e0bf9d257e44470d3331637fce27c28e4a1e3
Li Dong <lidongbest5@gmail.com>
2019-07-21 13:31:51 +0800

    update event api code

--- src/index.js
+++ src/index.js
@@ -33,88 +33,298 @@ export default class SDK {
   }
 
   /**
-   * pet's methods
+   * event's methods
    */
-  pet = {
+  event = {
     /**
-     * List all pets
+     * List all events
      *
-     * @param {ListPetsRequest} req listPets request
-     * @returns {Promise<ListPetsResponse>} A paged array of pets
+     * @param {ListEventsRequest} req listEvents request
+     * @returns {Promise<ListEventsResponse>} A paged array of events
      */
-    listPets: (req = {}) => {
+    listEvents: (req = {}) => {
       const { query, headers } = req;
 
-      return fetch(`${this.base}/pets`, {
-        method: "get",
+      return fetch(`${this.base}/events`, {
+        method: "GET",
         query: denormalize(query),
         headers: { Authorization: this.auth, ...headers },
       });
     },
     /**
-     * Create a pet
+     * Create an event
      *
-     * @param {CreatePetRequest} req createPet request
-     * @returns {Promise<CreatePetResponse>} The Pet created
+     * @param {CreateEventRequest} req createEvent request
+     * @returns {Promise<CreateEventResponse>} The Event created
      */
-    createPet: (req = {}) => {
+    createEvent: (req = {}) => {
       const { headers, body } = req;
 
-      if (!body) throw new Error("requetBody is required for createPet");
+      if (!body) throw new Error("requetBody is required for createEvent");
 
-      return fetch(`${this.base}/pets`, {
-        method: "post",
+      return fetch(`${this.base}/events`, {
+        method: "POST",
         body,
         headers: { Authorization: this.auth, ...headers },
       });
     },
     /**
-     * Find pet by id
+     * Get event by id
      *
-     * @param {ShowPetByIdRequest} req showPetById request
-     * @returns {Promise<ShowPetByIdResponse>} Expected response to a valid request
+     * @param {GetEventRequest} req getEvent request
+     * @returns {Promise<GetEventResponse>} The event with given id
      */
-    showPetById: (req = {}) => {
-      const { petId, headers } = req;
+    getEvent: (req = {}) => {
+      const { eventId, headers } = req;
 
-      if (!petId) throw new Error("petId is required for showPetById");
+      if (!eventId) throw new Error("eventId is required for getEvent");
 
-      return fetch(`${this.base}/pets/${petId}`, {
-        method: "get",
+      return fetch(`${this.base}/events/${eventId}`, {
+        method: "GET",
         headers: { Authorization: this.auth, ...headers },
       });
     },
     /**
-     * Update pet
+     * Update event
      *
-     * @param {UpdatePetRequest} req updatePet request
-     * @returns {Promise<UpdatePetResponse>} The pet
+     * @param {UpdateEventRequest} req updateEvent request
+     * @returns {Promise<UpdateEventResponse>} The event
      */
-    updatePet: (req = {}) => {
-      const { petId, headers, body } = req;
+    updateEvent: (req = {}) => {
+      const { eventId, headers, body } = req;
 
-      if (!petId) throw new Error("petId is required for updatePet");
-      if (!body) throw new Error("requetBody is required for updatePet");
+      if (!eventId) throw new Error("eventId is required for updateEvent");
+      if (!body) throw new Error("requetBody is required for updateEvent");
 
-      return fetch(`${this.base}/pets/${petId}`, {
-        method: "put",
+      return fetch(`${this.base}/events/${eventId}`, {
+        method: "PUT",
         body,
         headers: { Authorization: this.auth, ...headers },
       });
     },
     /**
+     * 删除指定活动
      *
-     *
-     * @param {DeletePetRequest} req deletePet request
-     * @returns {Promise<DeletePetResponse>} pet deleted
+     * @param {DeleteEventRequest} req deleteEvent request
+     * @returns {Promise<DeleteEventResponse>} event deleted
      */
-    deletePet: (req = {}) => {
-      const { petId, headers } = req;
+    deleteEvent: (req = {}) => {
+      const { eventId, headers } = req;
 
-      if (!petId) throw new Error("petId is required for deletePet");
+      if (!eventId) throw new Error("eventId is required for deleteEvent");
 
-      return fetch(`${this.base}/pets/${petId}`, {
-        method: "delete",
+      return fetch(`${this.base}/events/${eventId}`, {
+        method: "DELETE",
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+  };
+  /**
+   * module's methods
+   */
+  module = {
+    /**
+     * Update module
+     *
+     * @param {UpdateModuleRequest} req updateModule request
+     * @returns {Promise<UpdateModuleResponse>} The module
+     */
+    updateModule: (req = {}) => {
+      const { eventId, headers, body } = req;
+
+      if (!eventId) throw new Error("eventId is required for updateModule");
+      if (!body) throw new Error("requetBody is required for updateModule");
+
+      return fetch(`${this.base}/events/${eventId}/module`, {
+        method: "PUT",
+        body,
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+  };
+  /**
+   * register's methods
+   */
+  register = {
+    /**
+     * List all registers
+     *
+     * @param {ListRegistersRequest} req listRegisters request
+     * @returns {Promise<ListRegistersResponse>} A paged array of registers
+     */
+    listRegisters: (req = {}) => {
+      const { eventId, query, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for listRegisters");
+
+      return fetch(`${this.base}/events/${eventId}/registers`, {
+        method: "GET",
+        query: denormalize(query),
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Create an register
+     *
+     * @param {CreateRegisterRequest} req createRegister request
+     * @returns {Promise<CreateRegisterResponse>} The Register created
+     */
+    createRegister: (req = {}) => {
+      const { eventId, headers, body } = req;
+
+      if (!eventId) throw new Error("eventId is required for createRegister");
+      if (!body) throw new Error("requetBody is required for createRegister");
+
+      return fetch(`${this.base}/events/${eventId}/registers`, {
+        method: "POST",
+        body,
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Get register by id
+     *
+     * @param {GetRegisterRequest} req getRegister request
+     * @returns {Promise<GetRegisterResponse>} The register with given id
+     */
+    getRegister: (req = {}) => {
+      const { eventId, registerId, query, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for getRegister");
+      if (!registerId) throw new Error("registerId is required for getRegister");
+
+      return fetch(`${this.base}/events/${eventId}/registers/${registerId}`, {
+        method: "GET",
+        query: denormalize(query),
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Update register
+     *
+     * @param {UpdateRegisterRequest} req updateRegister request
+     * @returns {Promise<UpdateRegisterResponse>} The register
+     */
+    updateRegister: (req = {}) => {
+      const { eventId, registerId, headers, body } = req;
+
+      if (!eventId) throw new Error("eventId is required for updateRegister");
+      if (!registerId) throw new Error("registerId is required for updateRegister");
+      if (!body) throw new Error("requetBody is required for updateRegister");
+
+      return fetch(`${this.base}/events/${eventId}/registers/${registerId}`, {
+        method: "PUT",
+        body,
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * 删除指定表单
+     *
+     * @param {DeleteRegisterRequest} req deleteRegister request
+     * @returns {Promise<DeleteRegisterResponse>} register deleted
+     */
+    deleteRegister: (req = {}) => {
+      const { eventId, registerId, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for deleteRegister");
+      if (!registerId) throw new Error("registerId is required for deleteRegister");
+
+      return fetch(`${this.base}/events/${eventId}/registers/${registerId}`, {
+        method: "DELETE",
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+  };
+  /**
+   * attendee's methods
+   */
+  attendee = {
+    /**
+     * List all attendees
+     *
+     * @param {ListAttendeesRequest} req listAttendees request
+     * @returns {Promise<ListAttendeesResponse>} A paged array of attendees
+     */
+    listAttendees: (req = {}) => {
+      const { eventId, query, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for listAttendees");
+
+      return fetch(`${this.base}/events/${eventId}/attendees`, {
+        method: "GET",
+        query: denormalize(query),
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Create an attendee
+     *
+     * @param {CreateAttendeeRequest} req createAttendee request
+     * @returns {Promise<CreateAttendeeResponse>} The Attendee created
+     */
+    createAttendee: (req = {}) => {
+      const { eventId, headers, body } = req;
+
+      if (!eventId) throw new Error("eventId is required for createAttendee");
+      if (!body) throw new Error("requetBody is required for createAttendee");
+
+      return fetch(`${this.base}/events/${eventId}/attendees`, {
+        method: "POST",
+        body,
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Get attendee by id
+     *
+     * @param {GetAttendeeRequest} req getAttendee request
+     * @returns {Promise<GetAttendeeResponse>} The attendee with given id
+     */
+    getAttendee: (req = {}) => {
+      const { eventId, attendeeId, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for getAttendee");
+      if (!attendeeId) throw new Error("attendeeId is required for getAttendee");
+
+      return fetch(`${this.base}/events/${eventId}/attendees/${attendeeId}`, {
+        method: "GET",
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * Update attendee
+     *
+     * @param {UpdateAttendeeRequest} req updateAttendee request
+     * @returns {Promise<UpdateAttendeeResponse>} The Attendee
+     */
+    updateAttendee: (req = {}) => {
+      const { eventId, attendeeId, headers, body } = req;
+
+      if (!eventId) throw new Error("eventId is required for updateAttendee");
+      if (!attendeeId) throw new Error("attendeeId is required for updateAttendee");
+      if (!body) throw new Error("requetBody is required for updateAttendee");
+
+      return fetch(`${this.base}/events/${eventId}/attendees/${attendeeId}`, {
+        method: "PUT",
+        body,
+        headers: { Authorization: this.auth, ...headers },
+      });
+    },
+    /**
+     * 删除指定报名
+     *
+     * @param {DeleteAttendeeRequest} req deleteAttendee request
+     * @returns {Promise<DeleteAttendeeResponse>} attendee deleted
+     */
+    deleteAttendee: (req = {}) => {
+      const { eventId, attendeeId, headers } = req;
+
+      if (!eventId) throw new Error("eventId is required for deleteAttendee");
+      if (!attendeeId) throw new Error("attendeeId is required for deleteAttendee");
+
+      return fetch(`${this.base}/events/${eventId}/attendees/${attendeeId}`, {
+        method: "DELETE",
         headers: { Authorization: this.auth, ...headers },
       });
     },
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -7,7 +7,10 @@ declare class SDK {
   token: string;
   auth: string;
 
-  pet: SDK.PetAPI;
+  event: SDK.EventAPI;
+  module: SDK.ModuleAPI;
+  register: SDK.RegisterAPI;
+  attendee: SDK.AttendeeAPI;
 }
 
 declare namespace SDK {
@@ -16,121 +19,496 @@ declare namespace SDK {
     token?: string;
   }
 
-  export interface PetAPI {
+  export interface EventAPI {
     /**
-     * List all pets
+     * List all events
      */
-    listPets(req: ListPetsRequest): Promise<ListPetsResponse>;
+    listEvents(req: ListEventsRequest): Promise<ListEventsResponse>;
     /**
-     * Create a pet
+     * Create an event
      */
-    createPet(req: CreatePetRequest): Promise<CreatePetResponse>;
+    createEvent(req: CreateEventRequest): Promise<CreateEventResponse>;
     /**
-     * Find pet by id
+     * Get event by id
      */
-    showPetById(req: ShowPetByIdRequest): Promise<ShowPetByIdResponse>;
+    getEvent(req: GetEventRequest): Promise<GetEventResponse>;
     /**
-     * Update pet
+     * Update event
      */
-    updatePet(req: UpdatePetRequest): Promise<UpdatePetResponse>;
+    updateEvent(req: UpdateEventRequest): Promise<UpdateEventResponse>;
     /**
-     *
+     * 删除指定活动
      */
-    deletePet(req: DeletePetRequest): Promise<DeletePetResponse>;
+    deleteEvent(req: DeleteEventRequest): Promise<DeleteEventResponse>;
+  }
+  export interface ModuleAPI {
+    /**
+     * Update module
+     */
+    updateModule(req: UpdateModuleRequest): Promise<UpdateModuleResponse>;
+  }
+  export interface RegisterAPI {
+    /**
+     * List all registers
+     */
+    listRegisters(req: ListRegistersRequest): Promise<ListRegistersResponse>;
+    /**
+     * Create an register
+     */
+    createRegister(req: CreateRegisterRequest): Promise<CreateRegisterResponse>;
+    /**
+     * Get register by id
+     */
+    getRegister(req: GetRegisterRequest): Promise<GetRegisterResponse>;
+    /**
+     * Update register
+     */
+    updateRegister(req: UpdateRegisterRequest): Promise<UpdateRegisterResponse>;
+    /**
+     * 删除指定表单
+     */
+    deleteRegister(req: DeleteRegisterRequest): Promise<DeleteRegisterResponse>;
+  }
+  export interface AttendeeAPI {
+    /**
+     * List all attendees
+     */
+    listAttendees(req: ListAttendeesRequest): Promise<ListAttendeesResponse>;
+    /**
+     * Create an attendee
+     */
+    createAttendee(req: CreateAttendeeRequest): Promise<CreateAttendeeResponse>;
+    /**
+     * Get attendee by id
+     */
+    getAttendee(req: GetAttendeeRequest): Promise<GetAttendeeResponse>;
+    /**
+     * Update attendee
+     */
+    updateAttendee(req: UpdateAttendeeRequest): Promise<UpdateAttendeeResponse>;
+    /**
+     * 删除指定报名
+     */
+    deleteAttendee(req: DeleteAttendeeRequest): Promise<DeleteAttendeeResponse>;
   }
 
-  type ListPetsRequest = {
+  type ListEventsRequest = {
     query: {
       limit?: number;
       offset?: number;
       sort?: string;
-      select?: string;
-      group?: string | [string];
+      select?: number;
 
       filter: {
-        tag?: string;
-        age: {
-          $gt?: number;
-        };
-        birthAt: {
+        startedAt: {
           $gt?: string;
           $lt?: string;
         };
-        grade: {
+        endAt: {
           $gt?: string;
           $lt?: string;
         };
+        name: {
+          $regex?: string;
+        };
+        ns?: string;
       };
     };
   };
 
-  type ListPetsResponse = {
-    body: [Pet];
+  type ListEventsResponse = {
+    body: [Event];
     headers: {
-      xTotalCount: number;
+      xTotalCount: string;
     };
   };
 
-  type CreatePetRequest = {
-    body: PetDoc;
+  type CreateEventRequest = {
+    body: Event;
   };
 
-  type CreatePetResponse = {
-    body: Pet;
+  type CreateEventResponse = {
+    body: Event;
   };
 
-  type ShowPetByIdRequest = {
-    petId: string;
+  type GetEventRequest = {
+    eventId: string;
   };
 
-  type ShowPetByIdResponse = {
-    body: Pet;
+  type GetEventResponse = {
+    body: Event;
   };
 
-  type UpdatePetRequest = {
-    petId: string;
-    body: PetDoc;
+  type UpdateEventRequest = {
+    eventId: string;
+    body: Event;
   };
 
-  type UpdatePetResponse = {
-    body: Pet;
+  type UpdateEventResponse = {
+    body: Event;
   };
 
-  type DeletePetRequest = {
-    petId: string;
+  type DeleteEventRequest = {
+    eventId: string;
   };
 
-  type PetDoc = {
-    name: string;
-    tag: "DOG" | "CAT";
-    age: number;
-    birthAt: string;
-    grade: number;
+  type UpdateModuleRequest = {
+    eventId: string;
+    body: Module;
   };
-  type Pet = {
+
+  type UpdateModuleResponse = {
+    body: Module;
+  };
+
+  type ListRegistersRequest = {
+    eventId: string;
+
+    query: {
+      limit?: number;
+      offset?: number;
+      sort?: string;
+      select?: number;
+    };
+  };
+
+  type ListRegistersResponse = {
+    body: [Register];
+    headers: {
+      xTotalCount: string;
+    };
+  };
+
+  type CreateRegisterRequest = {
+    eventId: string;
+    body: Register;
+  };
+
+  type CreateRegisterResponse = {
+    body: Register;
+  };
+
+  type GetRegisterRequest = {
+    eventId: string;
+    registerId: string;
+
+    query: {
+      limit?: number;
+      offset?: number;
+      sort?: string;
+      select?: number;
+    };
+  };
+
+  type GetRegisterResponse = {
+    body: Register;
+  };
+
+  type UpdateRegisterRequest = {
+    eventId: string;
+    registerId: string;
+    body: Register;
+  };
+
+  type UpdateRegisterResponse = {
+    body: Register;
+  };
+
+  type DeleteRegisterRequest = {
+    eventId: string;
+    registerId: string;
+  };
+
+  type ListAttendeesRequest = {
+    eventId: string;
+
+    query: {
+      limit?: number;
+      offset?: number;
+      sort?: string;
+      select?: number;
+
+      filter: {
+        registerId?: string;
+        name?: string;
+        phone?: string;
+      };
+    };
+  };
+
+  type ListAttendeesResponse = {
+    body: [Attendee];
+    headers: {
+      xTotalCount: string;
+    };
+  };
+
+  type CreateAttendeeRequest = {
+    eventId: string;
+    body: Attendee;
+  };
+
+  type CreateAttendeeResponse = {
+    body: Attendee;
+  };
+
+  type GetAttendeeRequest = {
+    eventId: string;
+    attendeeId: string;
+  };
+
+  type GetAttendeeResponse = {
+    body: Attendee;
+  };
+
+  type UpdateAttendeeRequest = {
+    eventId: string;
+    attendeeId: string;
+    body: Attendee;
+  };
+
+  type UpdateAttendeeResponse = {
+    body: Attendee;
+  };
+
+  type DeleteAttendeeRequest = {
+    eventId: string;
+    attendeeId: string;
+  };
+
+  type Event = {
     id: string;
+    createdAt: string;
+    createdBy: string;
+    updatedAt: string;
+    updatedBy: string;
     name: string;
-    tag: "DOG" | "CAT";
-    age: number;
-    birthAt: string;
-    grade: number;
-  };
-  type PetOrDoc =
-    | {
-        name: string;
-        tag: "DOG" | "CAT";
-        age: number;
-        birthAt: string;
-        grade: number;
-      }
-    | {
+    startedAt: string;
+    endAt: string;
+    address: string;
+    desc: string;
+    bannerPc: string;
+    bannerMobile: string;
+    signinStartAt: string;
+    signinEndAt: string;
+    published: boolean;
+    publishedAt: string;
+    publishedBy: string;
+    ns: string;
+    siginin: [
+      {
         id: string;
         name: string;
-        tag: "DOG" | "CAT";
-        age: number;
-        birthAt: string;
-        grade: number;
-      };
+        createdAt: string;
+      }
+    ];
+    i18n: [
+      {
+        lan: string;
+        content: {};
+      }
+    ];
+    modules: [
+      {
+        id: string;
+        name: string;
+        type: string;
+        body:
+          | [
+              {
+                date: string;
+                start: string;
+                end: string;
+                subject: string;
+                speakers: string;
+                files: [
+                  {
+                    id: string;
+                    name: string;
+                    url: string;
+                  }
+                ];
+              }
+            ]
+          | [
+              {
+                avatar: string;
+                name: string;
+                position: string;
+                intro: string;
+              }
+            ]
+          | [
+              {
+                id: string;
+                name: string;
+                url: string;
+              }
+            ]
+          | {
+              content: {};
+            };
+        i18n: [
+          {
+            lan: string;
+            content: {};
+          }
+        ];
+      }
+    ];
+  };
+  type Module = {
+    id: string;
+    name: string;
+    type: string;
+    body:
+      | [
+          {
+            date: string;
+            start: string;
+            end: string;
+            subject: string;
+            speakers: string;
+            files: [
+              {
+                id: string;
+                name: string;
+                url: string;
+              }
+            ];
+          }
+        ]
+      | [
+          {
+            avatar: string;
+            name: string;
+            position: string;
+            intro: string;
+          }
+        ]
+      | [
+          {
+            id: string;
+            name: string;
+            url: string;
+          }
+        ]
+      | {
+          content: {};
+        };
+    i18n: [
+      {
+        lan: string;
+        content: {};
+      }
+    ];
+  };
+  type Agenda = {
+    date: string;
+    start: string;
+    end: string;
+    subject: string;
+    speakers: string;
+    files: [
+      {
+        id: string;
+        name: string;
+        url: string;
+      }
+    ];
+  };
+  type Guest = {
+    avatar: string;
+    name: string;
+    position: string;
+    intro: string;
+  };
+  type Custom = {
+    content: {};
+  };
+  type Language = {
+    lan: string;
+    content: {};
+  };
+  type File = {
+    id: string;
+    name: string;
+    url: string;
+  };
+  type Register = {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    eventId: string;
+    name: string;
+    code: string;
+    fields: [
+      {
+        basic: boolean;
+        required: boolean;
+        key: string;
+        type: string;
+        title: string;
+        placeholder: string;
+        options: [
+          {
+            label: string;
+            value: string;
+          }
+        ];
+      }
+    ];
+    i18n: [
+      {
+        lan: string;
+        content: {};
+      }
+    ];
+  };
+  type Field = {
+    basic: boolean;
+    required: boolean;
+    key: string;
+    type: string;
+    title: string;
+    placeholder: string;
+    options: [
+      {
+        label: string;
+        value: string;
+      }
+    ];
+  };
+  type Attendee = {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    eventId: string;
+    registerId: string;
+    code: string;
+    name: string;
+    gender: string;
+    industry: string;
+    company: string;
+    position: string;
+    department: string;
+    phone: string;
+    email: string;
+    extra: [
+      {
+        key: string;
+        value: string;
+      }
+    ];
+    signin: [
+      {
+        id: string;
+        name: string;
+        time: string;
+        signed: boolean;
+      }
+    ];
+  };
   type Err = {
     code: string;
     message: string;
